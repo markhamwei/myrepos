4f19677839b82ae145b4c3b0d865f74ab1b47488
markhamwei <129559892+markhamwei@users.noreply.github.com>
2023-03-31 18:48:37 -0400

    Add files via upload

---
+++ myhome/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+#import wolframalpha
+
+# Create your views here.
+def my_page(request):
+	# Replace YOUR_APP_ID with your Wolfram API App ID
+	#client = wolframalpha.Client("GLYJRW-TEH7UK7VYL")
+
+	# Get user input
+	#query = "solve (x+3)*6=8*x, x>0"
+
+	# Send query to Wolfram API
+	#res = client.query(query)
+
+	# Print the result
+	#print(next(res.results).text)	
+	#return HttpResponse(next(res.results).text)
+	return render(request, 'myhome.html')
