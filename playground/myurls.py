6b1b5e61e5dc5d0d8b41378de62bcf3fdbe3f45e
markhamwei <markhamwei@gmail.com>
2023-04-01 22:30:37 -0400

    first interact page

--- playground/myurls.py
+++ playground/myurls.py
@@ -3,5 +3,7 @@ from . import views
 
 # URLConf
 urlpatterns =[
-	path('hello/', views.say_hello)
+	path('hello/', views.say_hello),
+	path('query/', views.my_query),
+	path('calculate/', views.calculate)
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseBadRequest
 import wolframalpha
+import json
 
 # Create your views here.
 def say_hello(request):
@@ -17,3 +18,29 @@ def say_hello(request):
 	#print(next(res.results).text)	
 	return HttpResponse(next(res.results).text)
 	#return HttpResponse("Hello, what's up!")
+
+def calculate(request):
+	# Replace YOUR_APP_ID with your Wolfram API App ID
+	client = wolframalpha.Client("GLYJRW-TEH7UK7VYL")
+	context = {'content':'', 'year':'2023'}
+
+	#request_body = request.body
+	
+	try:
+		# Extract the values from the dictionary
+		question = request.GET['question']
+		
+		# Send query to Wolfram API
+		res = client.query(question)
+		context['content'] = next(res.results).text
+		
+		# Return a response
+		return render(request, 'answer.html', context)
+	
+	except Exception:
+		# Return a bad request response if the request data cannot be decoded or parsed
+		context['content'] = 'Invalid query string'
+		return render(request, 'answer.html', context)
+
+def my_query(request):
+	return render(request, 'query.html')
