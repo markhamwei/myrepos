a2a557132086768b92e5b59e8b09a7432901d209
markhamwei <markhamwei@gmail.com>
2023-04-14 22:04:15 -0400

    Update grade 5 time

--- grade5/time/timeviews.py
+++ grade5/time/timeviews.py
@@ -14,24 +14,33 @@ def time_page(request):
     except Exception:
         state = 0
     if (state == 0):  # the initial state
+        tmpMsec = myutil.get_msec()
+        if (tmpMsec % 2 == 1):
+            context['type'] = 'picture'
+        else:
+            context['type'] = 'text'
         context['state'] = '1'
         randoms = myutil.get_randoms(6, 0, 59)
         hour1 = randoms[0] % 12 + 1
         minute1 = randoms[1]
         second1 = randoms[2]
         context['hour1'] = hour1
-        context['minute1'] = minute1
-        context['second1'] = second1
+        context['minute1'] = f'{minute1:02d}'
+        context['second1'] = f'{second1:02d}'
         startTime = datetime.datetime(2023, 4, 11, hour1, minute1, second1)
-        estra_hour2 = randoms[3] % 3
+        estra_hour2 = randoms[3] % 6
         estra_minute2 = randoms[4]
         estra_second2 = randoms[5]
         extra_time = datetime.timedelta(
             hours=estra_hour2, minutes=estra_minute2, seconds=estra_second2)
         endTime = startTime + extra_time
         context['hour2'] = endTime.hour
-        context['minute2'] = endTime.minute
-        context['second2'] = endTime.second
+        context['hour1_12'] = startTime.strftime("%I")
+        context['hour2_12'] = endTime.strftime("%I")
+        context['minute2'] = f'{endTime.minute:02d}'
+        context['second2'] = f'{endTime.second:02d}'
+        context['am_pm1'] = startTime.strftime("%p")
+        context['am_pm2'] = endTime.strftime("%p")
         context['answer1'] = ''
         context['answer2'] = ''
         context['answer3'] = ''
@@ -41,6 +50,7 @@ def time_page(request):
         return render(request, 'grade5Clock.html', context)
     if (state == 1):  # which means user cliked submit in the initial page
         submittedValue = request.GET['Submit']
+        context['type'] = request.GET['type']
         hour1 = request.GET['hour1']
         minute1 = request.GET['minute1']
         second1 = request.GET['second1']
@@ -51,14 +61,16 @@ def time_page(request):
         answer2 = request.GET['answer2']
         answer3 = request.GET['answer3']
         context['hour1'] = hour1
-        context['minute1'] = minute1
-        context['second1'] = second1
+        context['minute1'] = f'{int(minute1):02d}'
+        context['second1'] = f'{int(second1):02d}'
         context['hour2'] = hour2
-        context['minute2'] = minute2
-        context['second2'] = second2
+        context['minute2'] = f'{int(minute2):02d}'
+        context['second2'] = f'{int(second2):02d}'
         try:
             startTime = datetime.datetime(
                 2023, 4, 11, int(hour1), int(minute1), int(second1))
+            context['hour1_12'] = startTime.strftime("%I")
+            context['am_pm1'] = startTime.strftime("%p")
             if (submittedValue != 'GetAnswer'):
                 extra_time = datetime.timedelta(
                     hours=int(answer1), minutes=int(answer2), seconds=int(answer3))
@@ -67,6 +79,8 @@ def time_page(request):
                     (endTime.hour == int(hour2) and endTime.minute == int(minute2) and endTime.second == int(second2))):
                 endTime = datetime.datetime(
                     2023, 4, 11, int(hour2), int(minute2), int(second2))
+                context['hour2_12'] = endTime.strftime("%I")
+                context['am_pm2'] = endTime.strftime("%p")
                 extra_time = endTime - startTime
                 hours, remainder = divmod(extra_time.seconds, 3600)
                 minutes, seconds = divmod(remainder, 60)
@@ -77,6 +91,10 @@ def time_page(request):
                 context['cmd'] = 'Next'
                 context['state'] = '0'
             else:
+                endTime = datetime.datetime(
+                    2023, 4, 11, int(hour2), int(minute2), int(second2))
+                context['hour2_12'] = endTime.strftime("%I")
+                context['am_pm2'] = endTime.strftime("%p")
                 context['answer1'] = answer1
                 context['answer2'] = answer2
                 context['answer3'] = answer3
@@ -90,6 +108,10 @@ def time_page(request):
             context['flag'] = 'x'
             context['state'] = '1'
             context['cmd'] = 'Retry'
+            endTime = datetime.datetime(
+                2023, 4, 11, int(hour2), int(minute2), int(second2))
+            context['hour2_12'] = endTime.strftime("%I")
+            context['am_pm2'] = endTime.strftime("%p")
 
         if (context['cmd'] == 'Retry'):
             context['button2'] = 'GetAnswer'
