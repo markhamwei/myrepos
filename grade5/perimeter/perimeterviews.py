c24887fe6c0caaabb4e722a8ef77ab16fa381b1f
mwl <maxwli@outlook.com>
2023-04-23 21:34:57 -0400

    Finished polygondrawer script and set up perimeter page

--- grade5/perimeter/perimeterviews.py
+++ grade5/perimeter/perimeterviews.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mylib import myutil
-import datetime
+from datetime import datetime
 
 # Create your views here.
 
 def perimeter_page(request):
-	context = {}
+	context = { 'year' : '2023' }
 	return render(request, 'grade5Perimeter.html', context)
--- static/PolygonDrawer.js
+++ static/PolygonDrawer.js
@@ -2,12 +2,14 @@
 const DrawingScope = (function() {
 	
 	// A binary search helper function
-	// 
+	// finds points along the circumference of a circle with a given radius
+	//   such that the side lengths of the traced polygon matches up with
+	//   the side lengths in the array
 	function find(array, rad) {
 		var ang = 0;
 		var ret = new Array(array.length);
 		for(let i = 0; i < array.length; i++) {
-			let cur = Math.acos(1-array[i]/2/rad);
+			let cur = Math.acos(1-array[i]*array[i]/2/rad/rad);
 			ang += cur;
 			ret[i] = [rad * Math.cos(ang), rad * Math.sin(ang)];
 		}
@@ -20,7 +22,11 @@ const DrawingScope = (function() {
 	function fit(array, rad) {
 		var ang = 0;
 		for(let i = 0; i < array.length; i++) {
-			let cur = Math.acos(1-array[i]/2/rad);
+			let cur = 1-array[i]*array[i]/2/rad/rad;
+			if(cur < -1) {
+				return true;
+			}
+			cur = Math.acos(cur);
 			ang += cur;
 		}
 		return ang >= 2*Math.PI;
@@ -43,7 +49,7 @@ const DrawingScope = (function() {
 		}
 		if(2*highest >= sum) {
 			//in this case, it's impossible to draw a polygon, so we do nothing.
-			console.log('impossible');
+			alert('An internal error has occurred. Please reload the page')
 			return;
 		}
 		if((1+Math.PI/4)*highest >= sum) {
@@ -61,12 +67,13 @@ const DrawingScope = (function() {
 					maxrad = rad;
 				}
 			}
-			let poly = find(array, minrad);
+			let poly = find(array, maxrad);
+			console.log(maxrad);
 			for(let i = 0; i < poly.length; i++) {
 				poly[i][0] = 200+poly[i][0]/minrad*150;
 				poly[i][1] = 200+poly[i][1]/minrad*150;
 			}
-			tracePoints(poly, canvasID);
+			tracePoints(array, poly, canvasID);
 		}
 	}
 	
@@ -90,6 +97,8 @@ const DrawingScope = (function() {
 		}
 		return Math.sqrt(x*x+y*y) >= array[idx];
 	}
+	// Takes in an array, the index of its highest element, and an angle value
+	// returns an array of vectors created by the process described in specialfit
 	function specialfind(array, idx, ang) {
 		let cur = 0;
 		let x = 0, y = 0;
@@ -109,6 +118,8 @@ const DrawingScope = (function() {
 		return ret;
 	}
 	
+	// drawPoly delegates its task to this function under a special case
+	// (1+Math.PI/4)*highest >= sum
 	function specialcase(array, idx, canvasID) {
 		let minang = 0, maxang = 2*PI/(array.length-1);
 		for(let it = 0; it < 30; it++) {
@@ -143,16 +154,18 @@ const DrawingScope = (function() {
 			poly[i][0] = poly[i][0]/maxlen*200+200;
 			poly[i][1] = poly[i][1]/maxlen*200+200;
 		}
-		tracePoints(poly, canvasID);
+		tracePoints(array, poly, canvasID);
 	}
 	
 	// Function to draw onto the canvas.
 	// Takes in an array of 2d vectors, each vector representing a point, and a canvas ID
 	// Resizes the canvas to 400x400 and the polygon onto the canvas
-	function tracePoints(points, canvasID) {
+	// Additionally draws the side lengths of the polygons
+	function tracePoints(array, points, canvasID) {
 		var canvas = document.getElementById(canvasID);
 		canvas.setAttribute('width', '400');
 		canvas.setAttribute('height', '400');
+
 		var context = canvas.getContext('2d');
 			console.log(points); //DEBUG
 		context.clearRect(0, 0, canvas.width, canvas.height);
@@ -163,6 +176,16 @@ const DrawingScope = (function() {
 		}
 		context.closePath();
 		context.stroke();
+
+		context.fillText(array[0].toString(), 
+			((points[0][0]+points[points.length-1][0])/2-200)*1.20+195,
+			((points[0][1]+points[points.length-1][1])/2-200)*1.20+210);
+
+		for(let i = 1; i < points.length; i++) {
+			context.fillText(array[i].toString(),
+				((points[i][0]+points[i-1][0])/2-200)*1.20+195,
+				((points[i][1]+points[i-1][1])/2-200)*1.20+210);
+		}
 	}
 	return {
 		// The only public function.
@@ -171,7 +194,18 @@ const DrawingScope = (function() {
 		generatePoly: function(canvasID) {
 			let len = Math.floor(Math.random()*7)+3;
 			let arr = new Array(len);
-			for(let i = 0; i < len; i++) arr[i] = Math.floor(Math.random()*10+1.1)*10;
+			let max = -1, maxidx = 0, sum = 0;
+			for(let i = 0; i < len; i++) {
+				arr[i] = Math.floor(Math.random()*10+1.1);
+				if(arr[i] > max) {
+					max = arr[i];
+					maxidx = i;
+				}
+				sum += arr[i];
+			}
+			if(2*max >= sum) {
+				arr[maxidx] = sum-max-1; //this is to ensure the polygon will always be possible to draw
+			}
 				console.log(arr); //DEBUG
 			drawPoly(arr, canvasID);
 		}
