4e50525c2a2f858d37eb9ec7c57bcfc08e5dca5c
mwl <maxwli@outlook.com>
2023-05-27 15:51:39 -0400

    added profile model with one-to-one relation to user

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.contrib import admin
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -14,4 +17,13 @@ class QuestionResponse(models.Model):
 	correctness = models.BooleanField()	# Whether or not response was correct
 	response_peeked = models.BooleanField() # whether or not Get Answer was used
 	response_datetime = models.DateTimeField() # datetime at which response was made
-	
+
+admin.site.register(QuestionResponse)
+
+
+class Profile(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	registration_date = models.DateTimeField(default=None)
+	is_premium = models.BooleanField(default=False)
+
+admin.site.register(Profile)
