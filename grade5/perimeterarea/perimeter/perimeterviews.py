ddce788261344805d60a0e2713c15f13c3d69f32
mwl <maxwli@outlook.com>
2023-06-06 17:41:29 -0400

    Updated perimeter page to use PolyDrawer

--- grade5/perimeterarea/perimeter/perimeterviews.py
+++ grade5/perimeterarea/perimeter/perimeterviews.py
@@ -11,20 +11,33 @@ import datetime
 
 @ensure_csrf_cookie
 def perimeter_page(request):
-	if(request.method == 'GET'):
-		context = { 'year' : '2023' }
-		return render(request, 'new/grade5Perimeter.html', context)
-	else:
-		if(request.user.is_authenticated):
-			content = json.loads(request.body)
-			field = QuestionResponse.objects.create(
-				user=request.user,
-				question_ID="grade5/perimeterarea/perimeter",
-				question_content=content['question-content'],
-				response=content['response'],
-				correctness=content['correctness'],
-				response_peeked=content['response-peeked'],
-				response_datetime=datetime.datetime.now()
-			)
-			field.save()
-		return render(request, 'new/grade5Perimeter.html')
+	context = {
+		'year' : '2023'
+	}
+	if(request.method == 'GET' or (request.method == 'POST' and request.POST['submit'] == 'Next Question!')):
+		context['state'] = 'question'
+		lst = myutil.get_randoms(2, 1, 10)
+		lst.append(myutil.get_random(1, min(sum(lst)-1, 10)))
+		context['question'] = f'PolyDrawer.drawPolygon_l({lst}, true, 35, "polygonCanvas")'
+		context['answer'] = myutil.scramble(str(sum(lst)))
+	
+	if(request.method == 'POST'):
+		if(request.POST['submit'] == 'Submit!'):
+			val = myutil.unscramble(request.POST['answer'])
+			resp = request.POST['response']
+			if(resp.isnumeric() and abs(float(val)-float(resp))<1e-7):
+				context['state'] = 'correct'
+				context['response'] = request.POST['response']
+				context['question'] = request.POST['question']
+				context['answer'] = request.POST['answer']
+			else:
+				context['state'] = 'incorrect'
+				context['response'] = request.POST['response']
+				context['question'] = request.POST['question']
+				context['answer'] = request.POST['answer']
+		if(request.POST['submit'] == 'Get Answer'):
+			context['state'] = 'get answer'
+			context['answer'] = myutil.unscramble(request.POST['answer'])
+			context['question'] = request.POST['question']
+
+	return render(request, 'new/grade5Perimeter.html', context)
